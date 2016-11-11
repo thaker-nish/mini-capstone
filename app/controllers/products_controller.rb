9cc9216b64b3afeb7fe2c4183d3fb930406ae94b
thaker-nish <thaker.nish@gmail.com>
2016-11-10 18:53:31 -0600

    Add a third product and individual product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,40 @@
 class ProductsController < ApplicationController
   def product_list_method
- @product_all = Product.all
+    @product_all = Product.all
     render 'product_list.html.erb'
+  end
+
+  def product1_method
+    @product1 = Product.first
+    @product1_name = @product1.name
+    @product1_price = @product1.price
+    @product1_image = @product1.image
+    @product1_description = @product1.description
+    render 'product1.html.erb'
+  end
+    def product2_method
+    @product2 = Product.second
+    @product2_name = @product2.name
+    @product2_price = @product2.price
+    @product2_image = @product2.image
+    @product2_description = @product2.description
+    render 'product2.html.erb'
+  end
+
+    def product3_method
+    @product3 = Product.third
+    @product3_name = @product3.name
+    @product3_price = @product3.price
+    @product3_image = @product3.image
+    @product3_description = @product3.description
+    render 'product3.html.erb'
+  end
+
+  def new_product_method
+    render 'new_product.html.erb'
+  end
+
+  def add_new_product_method
+    render 'add_new_product.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/product_list_url' => 'products#product_list_method'
+  get '/product1_url' => 'products#product1_method'
+  get '/product2_url' => 'products#product2_method'
+  get '/product3_url' => 'products#product3_method'
+  get '/new_product_form_url' => 'products#new_product_method'
+  post 'add_new_product_url' => 'products#add_new_product_method'
 end
