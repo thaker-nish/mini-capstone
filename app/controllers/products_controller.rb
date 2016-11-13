590136e0471adb292d4605ea8b60be4566c4577c
thaker-nish <thaker.nish@gmail.com>
2016-11-13 10:46:51 -0600

    Add a create method, add some bootstrap to form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,40 +1,26 @@
 class ProductsController < ApplicationController
-  def product_list_method
+  def index
     @product_all = Product.all
     render 'product_list.html.erb'
   end
 
-  def product1_method
-    @product1 = Product.first
-    @product1_name = @product1.name
-    @product1_price = @product1.price
-    @product1_image = @product1.image
-    @product1_description = @product1.description
-    render 'product1.html.erb'
-  end
-    def product2_method
-    @product2 = Product.second
-    @product2_name = @product2.name
-    @product2_price = @product2.price
-    @product2_image = @product2.image
-    @product2_description = @product2.description
-    render 'product2.html.erb'
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.html.erb'
   end
 
-    def product3_method
-    @product3 = Product.third
-    @product3_name = @product3.name
-    @product3_price = @product3.price
-    @product3_image = @product3.image
-    @product3_description = @product3.description
-    render 'product3.html.erb'
+  def new
+    render 'new.html.erb'
   end
 
-  def new_product_method
-    render 'new_product.html.erb'
-  end
-
-  def add_new_product_method
-    render 'add_new_product.html.erb'
+  def create
+    product = Product.new(
+      name: params['name'],
+      image: params['image'],
+      price: params['price'],
+      description: params['description']
+    )
+    product.save
+    render 'create.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/product_list_url' => 'products#product_list_method'
-  get '/product1_url' => 'products#product1_method'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
   get '/product2_url' => 'products#product2_method'
   get '/product3_url' => 'products#product3_method'
-  get '/new_product_form_url' => 'products#new_product_method'
-  post 'add_new_product_url' => 'products#add_new_product_method'
+  get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products'
 end
