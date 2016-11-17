37739842e9c8ab004d3c5be71eea3628cbebf237
thaker-nish <thaker.nish@gmail.com>
2016-11-16 21:24:24 -0600

    Create migration to change description to text, and another to change price from string to integer/decimal.

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109021947) do
+ActiveRecord::Schema.define(version: 20161117030500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
+    t.decimal  "price",       precision: 10, scale: 2
     t.string   "image"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
 end
