c38675f12d426614106aa7e96557d0503875d2bd
thaker-nish <thaker.nish@gmail.com>
2016-11-28 20:36:46 -0600

    Create category and CategoryProduct models and connect category CategoryProduct and product models together to link data(using a has_many, through).

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
   def sale_message
     if price.to_i <= 2
       'Discount item!'
